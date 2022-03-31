8870b055ba1e539daece7fbd048a86a62abada10
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2022-03-31 10:58:51 -0700

    Simple integer calculator

--- main.cpp
+++ main.cpp
@@ -7,209 +7,6 @@ Code By: Rajdeep Dhillon
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <cassert>
-
-using namespace std;
-
-void PrintGreetings();
-void PrintByeBye();
-void PrintMenuError();
-void PrintCurrentValue(int currentValue);
-int MenuChoice();
-int InputAnotherValue();
-int Add(int num1, int num2);
-int Subtract(int num1, int num2);
-int Multiply(int num1, int num2);
-int Divide(int num1, int num2);
-int Modulo(int num1, int num2);
-void ErrorCheck(int& value);
-int Sqrt(int num1);
-
-
-int main()
-{
-    int errorCheckNum = -1;
-    int value = 5;
-    int choice;
-    PrintGreetings();
-    PrintCurrentValue(value);
-    choice = MenuChoice();
-
-    value = InputAnotherValue();    
-    cout << value << endl;
-    
-    PrintByeBye();
-    PrintMenuError();
-    ErrorCheck(errorCheckNum);
-
-    assert(Add(1, 5) == 6);
-    assert(Add(500, 900) == 1400);
-    assert(Add(0, 0) == 0);
-    assert(Add(1, 0) == 1);
-    assert(Add(-200, 100) == -100);
-    
-    assert(Subtract(1, 5) == -4);
-    assert(Subtract(999, 5) == 994);
-    assert(Subtract(-500, 5) == -505);
-    assert(Subtract(0, 0) == 0);
-    assert(Subtract(0, -6000) == 6000);
-    
-    assert(Multiply(1, 5) == 5);
-    assert(Multiply(0, 5) == 0);
-    assert(Multiply(0, 0) == 0);
-    assert(Multiply(900, 2) == 1800);
-    assert(Multiply(-5, 5) == -25);
-    
-    assert(Divide(1, 5) == 0);
-    assert(Divide(5, 5) == 1);
-    assert(Divide(600, 30) == 20);
-    assert(Divide(-25, 5) == -5);
-    assert(Divide(-1, -1) == 1);
-    
-    assert(Modulo(1, 5) == 1);
-    assert(Modulo(500, 27) == 14);
-    assert(Modulo(2700, 7) == 5);
-    assert(Modulo(0, 1) == 0);
-    assert(Modulo(5, 5) == 0);
-    
-    assert(Sqrt(1) == 1);
-    assert(Sqrt(9) == 3);
-    assert(Sqrt(0) == 0);
-    assert(Sqrt(81) == 9);
-    assert(Sqrt(4) == 2);
-
-    return 0;
-}
-
-void PrintGreetings() {
-    cout << "+";
-    cout << setfill('=') << setw(25) << "";
-    cout << "+" << endl;
-    
-    cout << "|/\\/\\Falcon Calculator/\\/\\|" << endl;
-    
-    cout << "+";
-    cout << setfill('=') << setw(25) << "";
-    cout << "+" << endl;
-}
-
-void PrintByeBye() {
-    cout << "Thank you for using Falcon Calculator!" << endl;
-    
-}
-
-void PrintMenuError() {
-    cout << "The choice is not valid. " << endl;
-    
-}
-
-void PrintCurrentValue(int currentValue) {
-    cout << "Current value: " << currentValue << endl;
-    
-}
-
-int MenuChoice() {
-    int choice;
-    cout << "1) Add    2) Subtract 3) Multiply " << endl;
-    cout << "4) Divide 5) Modulo   6) Square root " << endl;
-    cout << "7) Exit " << endl;
-    cout << "Choose Functionality: " << endl;
-    cin >> choice;
-    return choice;
-    
-}
-
-int InputAnotherValue() {
-    int secondValue;
-    cout << "Input another value: " << endl;
-    cin >> secondValue;
-    return secondValue;
-}
-
-int Add(int num1, int num2) {
-    return num1 + num2;
-    
-}
-
-int Subtract(int num1, int num2) {
-    return num1 - num2;
-    
-}
-
-int Multiply(int num1, int num2) {
-    return num1 * num2;
-    
-}
-
-int Divide(int num1, int num2) {
-    if (num2 != 0) {
-        return num1 / num2;
-    }
-    else {
-        return -1;
-    }
-}
-
-int Modulo(int num1, int num2) {
-    if (num2 != 0) {
-        return num1 % num2;
-    }
-    else {
-        return -1;
-    }
-}
-
-int Sqrt(int num1) {
-    return sqrt(num1);
-    
-}
-
-void ErrorCheck(int& value) {
-    if (value < 0 || value > 99999) {
-        cout << "Calculation Error, resetting..." << endl;
-        value = 0;
-    }
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Part 2) 
-
-/******************************************************************************
-
-Code By: Rajdeep Dhillon
-
-*******************************************************************************/
-
-#include <iostream>
-#include <iomanip>
-#include <cmath>
 
 using namespace std;
 
@@ -232,19 +29,19 @@ int main()
 {
     int value;
     int choice;
-    
+
     PrintGreetings();
-    
+
     cout << "Enter a value: " << endl;
     cin >> value;
     ErrorCheck(value);
-    
-    
-    
+
+
+
     do {
         PrintCurrentValue(value);
         choice = MenuChoice();
-        
+
         if(choice == 1) {
             int secondValue = InputAnotherValue();
             int answer = Add(value, secondValue);
@@ -253,8 +50,8 @@ int main()
                 if(answer != 0) {
                     cout << value << " + " << secondValue << " = " << answer << endl;
                     value = answer;
-                    
-                } 
+
+                }
             }
             else {
                     cout << "Answer is 0." << endl;
@@ -269,7 +66,7 @@ int main()
                 if(answer != 0) {
                     cout << value << " - " << secondValue << " = " << answer << endl;
                     value = answer;
-                    
+
                 }
             }
             else {
@@ -285,7 +82,7 @@ int main()
                 if(answer != 0) {
                     cout << value << " * " << secondValue << " = " << answer << endl;
                     value = answer;
-                    
+
                 }
             }
             else {
@@ -301,7 +98,7 @@ int main()
                 if(answer != 0) {
                     cout << value << " / " << secondValue << " = " << answer << endl;
                     value = answer;
-                    
+
                 }
             }
             else {
@@ -317,7 +114,7 @@ int main()
                 if(answer != 0) {
                     cout << value << " % " << secondValue << " = " << answer << endl;
                     value = answer;
-                    
+
                 }
             }
             else {
@@ -332,7 +129,7 @@ int main()
                 if(answer != 0) {
                     cout << "Sqrt of " << value << " = " << answer << endl;
                     value = answer;
-                    
+
                 }
             }
             else {
@@ -343,18 +140,18 @@ int main()
         else if(choice == 7) {
             break;
         }
-        
+
         else {
             PrintMenuError();
         }
-        
+
         cout << "Enter a value: " << endl;
         cin >> value;
         ErrorCheck(value);
-        
+
     } while (choice != 7);
      PrintByeBye();
-    
+
     return 0;
 }
 
@@ -362,27 +159,27 @@ void PrintGreetings() {
     cout << "+";
     cout << setfill('=') << setw(25) << "";
     cout << "+" << endl;
-    
-    cout << "|/\\/\\Falcon Calculator/\\/\\|" << endl;
-    
+
+    cout << "|/\\/\\Simple Calculator/\\/\\|" << endl;
+
     cout << "+";
     cout << setfill('=') << setw(25) << "";
     cout << "+" << endl;
 }
 
 void PrintByeBye() {
-    cout << "Thank you for using Falcon Calculator!" << endl;
-    
+    cout << "Thank you for using the Calculator!" << endl;
+
 }
 
 void PrintMenuError() {
     cout << "The choice is not valid. " << endl;
-    
+
 }
 
 void PrintCurrentValue(int currentValue) {
     cout << "Current value: " << currentValue << endl;
-    
+
 }
 
 int MenuChoice() {
@@ -393,7 +190,7 @@ int MenuChoice() {
     cout << "Choose Functionality: " << endl;
     cin >> choice;
     return choice;
-    
+
 }
 
 int InputAnotherValue() {
@@ -405,17 +202,17 @@ int InputAnotherValue() {
 
 int Add(int num1, int num2) {
     return num1 + num2;
-    
+
 }
 
 int Subtract(int num1, int num2) {
     return num1 - num2;
-    
+
 }
 
 int Multiply(int num1, int num2) {
     return num1 * num2;
-    
+
 }
 
 int Divide(int num1, int num2) {
@@ -438,7 +235,7 @@ int Modulo(int num1, int num2) {
 
 int Sqrt(int num1) {
     return sqrt(num1);
-    
+
 }
 
 void ErrorCheck(int& value) {
@@ -446,5 +243,5 @@ void ErrorCheck(int& value) {
         cout << "Calculation Error, resetting..." << endl;
         value = 0;
     }
-    
+
 }
